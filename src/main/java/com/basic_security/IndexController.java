4ee33e3486f5a957aee4182090ac405b74dbd81e
AidenRoh <colohrby@gmail.com>
2025-01-16 22:40:44 +0900

    feature: add controller for testing spring security of autoconfiguration

---
+++ src/main/java/com/basic_security/IndexController.java
@@ -0,0 +1,15 @@
+package com.basic_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
