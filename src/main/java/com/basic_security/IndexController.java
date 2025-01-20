bf03c06112284b17728cd5777c96a5a255214b3e
AidenRoh <colohrby@gmail.com>
2025-01-20 19:18:41 +0900

    feature: add example case of request cache

--- src/main/java/com/basic_security/IndexController.java
+++ src/main/java/com/basic_security/IndexController.java
@@ -1,18 +1,22 @@
 package com.basic_security;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String index(@PathParam("customParam") String customParam) {
+        if (customParam != null) {
+            return "customPage";
+        } else return "index";
     }
 
     @GetMapping("/loginPage")
