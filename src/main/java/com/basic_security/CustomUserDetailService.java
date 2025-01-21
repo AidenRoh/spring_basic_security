af2c9be006d485a4da4f538b94397faaf2702d61
AidenRoh <colohrby@gmail.com>
2025-01-21 22:51:57 +0900

    feature: add example code of user details

--- src/main/java/com/basic_security/CustomUserDetailService.java
+++ src/main/java/com/basic_security/CustomUserDetailService.java
@@ -1,15 +1,22 @@
 package com.basic_security;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return User.withUsername("user1").password("{noop}1234").roles("USER").build();
+
+        AccountDto accountDto = new AccountDto("user1",
+                "{noop}1234",
+                List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        return new CustomUserDetails(accountDto);
     }
 }
